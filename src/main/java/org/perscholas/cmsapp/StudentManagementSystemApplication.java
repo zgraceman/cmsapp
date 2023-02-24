84cb9f27d7c2aa0973bbc74d35c9275b3b348424
ZachGraceman <zach.graceman@gmail.com>
2023-02-24 15:40:43 -0600

    init commit for project cms app

---
+++ src/main/java/org/perscholas/cmsapp/StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.cmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
